025238abf7aad55e75893970eada9a537b6ec507
gvv2065 <vparadise@ya.ru>
2025-10-08 13:03:29 +0300

    login курьера

--- api/Api.py
+++ api/Api.py
@@ -9,10 +9,19 @@ class Api:
     @allure.step("api: получение списка заказов")
     def get_orders() -> requests.Response:
         response = requests.get(Url.ORDERS)
-        return response.json() 
+        return response
     
     @staticmethod
     @allure.step("api: создание курьера")
     def create_courier(courier: CourierRequest) -> requests.Response:
         response = requests.post(Url.COURIER, data=courier.__dict__)
+        return response
+    
+    @staticmethod
+    @allure.step("api: login курьера")
+    def login_courier(login, password) -> requests.Response:
+        response = requests.post(Url.COURIER_LOGIN,
+            data=json.dumps({"login":login, "password":password}),
+            headers={"Content-Type": "application/json"}
+        )
         return response
--- data.py
+++ data.py
@@ -3,3 +3,8 @@ class Url:
     BASE_API = "https://qa-scooter.praktikum-services.ru/api/v1"
     ORDERS = BASE_API  + "/orders"
     COURIER = BASE_API  + "/courier"
+    COURIER_LOGIN = COURIER + "/login"
+    
+class Courier:
+    LOGIN = 'gvv'
+    PWD = '1234'
